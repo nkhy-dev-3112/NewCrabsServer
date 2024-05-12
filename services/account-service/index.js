fdf480666bf75f697a9050e4c21fa6b1735b7802
bonheurisme <100709185+unhi-dev-7012@users.noreply.github.com>
2024-05-12 17:15:32 +0700

    feat: update Profile

--- services/account-service/index.js
+++ services/account-service/index.js
@@ -4,6 +4,7 @@ const indexController = {};
 const { Authenticate } = require('./authenticate');
 
 const auth = new Authenticate(null);
+const accountController = require('./accountController');
 
 // Greeting
 indexController.welcome = (req, res) => {
@@ -25,6 +26,15 @@ indexController.signup = async (req, res) => {
     const strategy = req.query.s;
     auth.setStrategy(strategy);
     await auth.signup(req, res);
+}
+indexController.updateProfile = async (req, res) => {
+    const data = {
+        fullname: req.body.fullname,
+        genderId: req.body.genderId,
+        id: req.session.user.uid
+    };
+    const val = await accountController.updateProfile(data);
+    res.json(val);
 }
 
 module.exports = indexController;
